b03f3546a7522339a1e49b6b5a692e8f7db16026
fab835 <fabioczamora@gmail.com>
2017-03-31 18:31:58 -0300

    First commit

---
+++ js/carrossel.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(function(){
+	$("#carrossel").jCarouselLite({
+		auto    : 1500,	
+		speed   : 2000,
+		visible : 3
+		
+	})	
+	$("#carrossel2").jCarouselLite({
+		auto    : 2000,	
+		speed   : 2000,
+		visible : 2
+		
+	})	
+})
